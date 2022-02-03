147108e3753f7a10535282635059250de89e5d4d
esperanzaWG <myfrick57@hotmail.com>
2022-02-03 15:29:54 -0500

    pagination for desktop and mobile, and other

--- src/app/pages/ahri-matchups/help-choose-equipment/results-help.component.ts
+++ src/app/pages/ahri-matchups/help-choose-equipment/results-help.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 // model
 import {AHRICombinations} from '../../../models/AHRICombinations.model';
@@ -16,18 +17,33 @@ export class ResultsHelpComponent implements OnInit {
   ahriCombinations: AHRICombinations[] = [];
   p: number =1;
  
+  rows!: number;
+  isMobile = this.deviceService.isMobile();
+  isDesktopDevice = this.deviceService.isDesktop();
+
 
   @Input('data') 
     set data( data:any){
       this.ahriCombinations = data;
     }
 
-  constructor(
-    public _ahriCombinationService: AHRICombinationService,
-  ) { }
-
-  ngOnInit(): void {
-  }
+    constructor(
+      public _ahriCombinationService: AHRICombinationService,
+      private deviceService: DeviceDetectorService
+    ) { }
+  
+    ngOnInit(): void {
+      this.Device();
+    }
+  
+    
+    Device(){
+      if(this.isMobile === true){
+        this.rows=3;
+      }else{
+        this.rows=10
+      }
+    }
 
 
 
