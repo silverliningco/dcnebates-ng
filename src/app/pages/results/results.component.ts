5ba7c74ac6bcc440b0ceab442c3a7a8750262124
Atixcarlos <atix.figueroa@gmail.com>
2022-03-03 22:52:12 -0500

    Results one component

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -5,48 +5,49 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 import {ThemePalette} from '@angular/material/core';
 
 // model
-import {AHRICombinations} from '../../../models/AHRICombinations.model';
-import { detailParams } from '../../../models/detail.model';
+import {AHRICombinations} from '../../models/AHRICombinations.model';
+import { detailParams } from '../../models/detail.model';
 
 // sevice
-import {AHRICombinationService} from '../../../services/AHRICombinations.service';
-import { paramsDetailService } from '../../../services/params-detail.service';
+import {AHRICombinationService} from '../../services/AHRICombinations.service';
+import { paramsDetailService } from '../../services/params-detail.service';
 
 @Component({
-  selector: 'app-results-hc',
-  templateUrl: './results-hc.component.html',
-  styleUrls: ['./results-hc.component.css']
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.css']
 })
-export class ResultsHCComponent implements OnInit {
+export class ResultsComponent implements OnInit {
 
+ 
   DetailParams: detailParams = new detailParams();
   loadDetailParams!: any;
 
   ahriCombinations: AHRICombinations[] = [];
-  p: number =1;
- 
+  p: number = 1;
+
   rows!: number;
   isMobile = this.deviceService.isMobile();
   isDesktopDevice = this.deviceService.isDesktop();
 
-
-  @Input('data') 
+  @Input('data')
     set data( data:any){
+      //console.log(data);
       this.ahriCombinations = data;
     }
 
-    constructor(
-      public _ahriCombinationService: AHRICombinationService,
-      private deviceService: DeviceDetectorService,
-      public _paramsDetailService: paramsDetailService
-    ) { }
-  
-    ngOnInit(): void {
-      this.Device();
+  constructor(
+    public _ahriCombinationService: AHRICombinationService,
+    private deviceService: DeviceDetectorService,
+    public _paramsDetailService: paramsDetailService
+  ) { }
 
-      this._paramsDetailService.sentParams
+  ngOnInit(): void {
+    this.Device();
+
+    this._paramsDetailService.sentParams
           .subscribe((forDetail: any) => {
-            //console.log(forDetail);
+            console.log(forDetail);
             //console.log(forDetail.data.country);
 
             // load data of forDetail to model paramsDetailService
@@ -58,23 +59,21 @@ export class ResultsHCComponent implements OnInit {
             this.DetailParams.state = forDetail.data.state;
             this.DetailParams.eligibilityDetail = forDetail.data.eligibilityDetail;
 
-            this.loadDetailParams =this.DetailParams;
+            this.loadDetailParams = this.DetailParams;
 
-            //console.log(this.DetailParams);
+            console.log(this.DetailParams);
 
           });
+  }
 
-    }
-  
-    
-    Device(){
-      if(this.isMobile === true){
-        this.rows=3;
-      }else{
-        this.rows=10
-      }
-    }
 
+  Device(){
+    if(this.isMobile === true){
+      this.rows=3;
+    }else{
+      this.rows=10;
+    }
+  }
 
   // MODAL For slider color and range
   color: ThemePalette = 'accent';
@@ -87,7 +86,5 @@ export class ResultsHCComponent implements OnInit {
     return value;
   }
   // MODAL end
-
-
 
 }
