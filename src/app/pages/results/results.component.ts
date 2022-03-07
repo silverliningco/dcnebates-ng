f914568171a4c7f85ccb226ecaa1a6dea3e4d081
Atixcarlos <atix.figueroa@gmail.com>
2022-03-07 17:02:03 -0500

    Improving pagination

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -2,14 +2,14 @@ import { Component, Input, OnInit } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
 // modal color para slider
-import {ThemePalette} from '@angular/material/core';
+import { ThemePalette } from '@angular/material/core';
 
 // model
-import {AHRICombinations} from '../../models/AHRICombinations.model';
+import { AHRICombinations } from '../../models/AHRICombinations.model';
 import { detailParams } from '../../models/detail.model';
 
 // sevice
-import {AHRICombinationService} from '../../services/AHRICombinations.service';
+import { AHRICombinationService } from '../../services/AHRICombinations.service';
 import { paramsDetailService } from '../../services/params-detail.service';
 
 @Component({
@@ -19,22 +19,23 @@ import { paramsDetailService } from '../../services/params-detail.service';
 })
 export class ResultsComponent implements OnInit {
 
- 
+
   DetailParams: detailParams = new detailParams();
   loadDetailParams!: any;
 
   ahriCombinations: AHRICombinations[] = [];
   p: number = 1;
-
+  beginning?: number;
+  end?: number;
   rows!: number;
   isMobile = this.deviceService.isMobile();
   isDesktopDevice = this.deviceService.isDesktop();
 
   @Input('data')
-    set data( data:any){
-      //console.log(data);
-      this.ahriCombinations = data;
-    }
+  set data(data: any) {
+    //console.log(data);
+    this.ahriCombinations = data;
+  }
 
   constructor(
     public _ahriCombinationService: AHRICombinationService,
@@ -46,32 +47,40 @@ export class ResultsComponent implements OnInit {
     this.Device();
 
     this._paramsDetailService.sentParams
-          .subscribe((forDetail: any) => {
-            console.log(forDetail);
-            //console.log(forDetail.data.country);
+      .subscribe((forDetail: any) => {
+        console.log(forDetail);
 
-            // load data of forDetail to model paramsDetailService
-            this.DetailParams.rebateIds = forDetail.data.rebateIds;
-            this.DetailParams.storeId = forDetail.data.storeId;
-            this.DetailParams.country = forDetail.data.country;
-            this.DetailParams.electricUtilityId = forDetail.data.electricUtilityId;
-            this.DetailParams.gasOilUtilityId = forDetail.data.gasOilUtilityId;
-            this.DetailParams.state = forDetail.data.state;
-            this.DetailParams.eligibilityDetail = forDetail.data.eligibilityDetail;
+        // load data of forDetail to model paramsDetailService
+        this.DetailParams.rebateIds = forDetail.data.rebateIds;
+        this.DetailParams.storeId = forDetail.data.storeId;
+        this.DetailParams.country = forDetail.data.country;
+        this.DetailParams.electricUtilityId = forDetail.data.electricUtilityId;
+        this.DetailParams.gasOilUtilityId = forDetail.data.gasOilUtilityId;
+        this.DetailParams.state = forDetail.data.state;
+        this.DetailParams.eligibilityDetail = forDetail.data.eligibilityDetail;
 
-            this.loadDetailParams = this.DetailParams;
+        this.loadDetailParams = this.DetailParams;
 
-            console.log(this.DetailParams);
+      });
 
-          });
+
+    this.ObtainPaginationText();
   }
 
-
-  Device(){
-    if(this.isMobile === true){
-      this.rows=3;
-    }else{
-      this.rows=10;
+  ObtainPaginationText() {
+    let totalPages = Math.ceil(this.ahriCombinations?.length / this.rows)
+    this.beginning = this.p === 1 ? 1 : this.rows * (this.p - 1) + 1;
+    this.end = this.p === totalPages ? this.ahriCombinations?.length : this.beginning + this.rows - 1
+  }
+  pageChanged(event: number) {
+    this.p = event;
+    this.ObtainPaginationText();
+  }
+  Device() {
+    if (this.isMobile === true) {
+      this.rows = 3;
+    } else {
+      this.rows = 10;
     }
   }
 
