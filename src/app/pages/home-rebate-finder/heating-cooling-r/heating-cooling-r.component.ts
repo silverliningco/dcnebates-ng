26ba84eaf4aeafa07eb4065e846c0dfe38c82cc9
Atixcarlos <atix.figueroa@gmail.com>
2022-05-13 12:49:43 -0500

    Removing rebates from tabs

--- src/app/pages/home-rebate-finder/heating-cooling-r/heating-cooling-r.component.ts
+++ src/app/pages/home-rebate-finder/heating-cooling-r/heating-cooling-r.component.ts
@@ -10,7 +10,6 @@ import { bridgeService } from '../../../services/bridge.service';
 import { ApiService } from 'src/app/services/api.service';
 
 import { utilityInfo } from '../../../models/utility';
-import { Rebate, RebateTier, Criteria } from '../../../models/rebate';
 
 @Component({
   selector: 'app-heating-cooling-r',
@@ -33,7 +32,6 @@ export class HeatingCoolingRComponent implements OnInit {
   nominalSizeGroup !: FormGroup;
   furnaceGroup !: FormGroup; stateGroup !: FormGroup;
   utilityGroup !: FormGroup;
-  availableRebatesGroup  !: FormGroup;
 
   payload: any;
 
@@ -43,9 +41,6 @@ export class HeatingCoolingRComponent implements OnInit {
   sendGasOil: Array<any> = [];
   myUtilityIds!: Array<any>;
   myState!: string;
-
-  availableRebates!: Array<Rebate>;
-  IsValidAvailabeRebates: boolean = true;
 
   /* display columns when they have data in table of results */
   showFurnace: boolean = true;
@@ -94,10 +89,6 @@ export class HeatingCoolingRComponent implements OnInit {
     this.utilityGroup = this._formBuilder.group({
       electricUtility: ['', Validators.required],
       gasOilUtility: ['', Validators.required]
-    });
-
-    this.availableRebatesGroup = this._formBuilder.group({
-
     });
 
     this.nominalSizeGroup = this._formBuilder.group({
@@ -235,251 +226,6 @@ export class HeatingCoolingRComponent implements OnInit {
 
   }
 
-  PrepareAvailableRebates(){
-    this.myUtilityIds = [
-      this.utilityGroup.controls['electricUtility'].value,
-      this.utilityGroup.controls['gasOilUtility'].value
-    ];
-
-    this.myState = this.stateGroup.controls['state'].value;
-
-    this.GetAvailableRebates(this.myState, this.myUtilityIds);
-  }
-
-  GetAvailableRebates(state: any, utilityIds: any) {
-    /*TODO: add elegibilty questions */
-    this._api.AvailableRebates(state, JSON.stringify(utilityIds),null).subscribe({
-      next: (resp) => {
-       this.processingAvailableRebates(resp);
-      },
-      error: (e) => alert(e.error),
-      complete: () => console.info('complete')
-    })
-  }
-
-  processingAvailableRebates(myResp: any){
-    this.availableRebates = [];
-    for (let indx = 0; indx < myResp.length; indx++) {
-      const reb = myResp[indx];
-
-      /* matches the level rebateCriteria in the defined model */
-      let myCriterias: Array<Criteria> = [];
-      reb.rebateCriteria?.forEach( (element: any) =>{
-        myCriterias.push({ title: element, completed: true });
-      });
-
-      /* matches the level RebateTier in the defined model */
-      let myTier: Array<RebateTier> = [];
-      reb.rebateTiers?.forEach( (element: any) => {
-
-        /* matches the level Tier Criterias in the defined model */
-        let myTierCriterias: Array<Criteria> = [];
-        element.rebateTierCriteria?.forEach((el: any) =>{
-          myTierCriterias.push({ title: el, completed: element.default });
-        });
-
-          myTier.push({
-          title: element.title,
-          rebateTierId: element.rebateTierId,
-          rebateTierCriteria: myTierCriterias,
-          completed: element.default,
-          defaultTier: element.default,
-
-        });
-        
-
-        
-      });
-
-      this.availableRebates.push({
-        title: reb.title,
-        rebateId: reb.rebateId,
-        rebateCriteria: myCriterias,
-        rebateTiers: myTier,
-        completed: true
-      });
-    }
-  }
-
-  /* Elegibility detail codes */
-  reb_tier_change(rebTier: RebateTier, reb: Rebate) {
-    rebTier.rebateTierCriteria?.forEach(element => {
-      element.completed = rebTier.completed!;
-    });
-
-    // If there are multiple rebate tiers in a given rebate,
-    // checking one rebate tier should always uncheck the remaining tier(s).
-    this.uncheckRemainingTiers(rebTier, reb);
-
-    // validate if at least one rebate is selected
-    this.validateSelection();
-  }
-
-  // If there are multiple rebate tiers in a given rebate,
-  // checking one rebate tier should always uncheck the remaining tier(s).
-  uncheckRemainingTiers(rebTier: RebateTier, reb: Rebate){
-
-
-    const resultTier = reb.rebateTiers?.filter(rt => rt.completed == true);
-    const resultTierCriteria = rebTier?.rebateTierCriteria?.filter(rtc => rtc.completed == true);
-    const resultCriteria = reb.rebateCriteria?.filter(rc => rc.completed == true);
-
-    if(resultTier!.length > 0 && resultCriteria!.length == reb.rebateCriteria?.length && resultTierCriteria!.length == rebTier.rebateTierCriteria!.length) {
-      reb.completed = true;
-    } else {
-      reb.completed = false;
-    }
-
-    reb.rebateTiers?.forEach(element => {
-
-      if( element.title != rebTier.title){
-        // Uncheck rebate tier.
-        element.completed = false;
-        element.rebateTierCriteria?.forEach(el2 => {
-          // Uncheck rebate tier criterias.
-          if(el2.completed)
-          el2.completed = false;
-        });
-      }
-    });
-  }
-
-  reb_tier_criteria_change(rebTier: RebateTier, reb: Rebate) {
-    let checked_count = 0;
-
-    //Get total checked items
-    rebTier.rebateTierCriteria?.forEach(element => {
-      if (element.completed)
-        checked_count++;
-    });
-
-    if (checked_count == rebTier.rebateTierCriteria!.length) {
-      //If checked count is equal to total items; then check the master checkbox.
-      rebTier.completed = true;
-
-      // If there are multiple rebate tiers in a given rebate,
-      // checking one rebate tier should always uncheck the remaining tier(s).
-      this.uncheckRemainingTiers(rebTier, reb);
-
-    } else {
-      
-      rebTier.completed = false;
-      
-      const resultTier = reb.rebateTiers?.filter(rt => rt.completed == true);
-      const resultTierCriteria = rebTier?.rebateTierCriteria?.filter(rtc => rtc.completed == true);
-      const resultCriteria = reb.rebateCriteria?.filter(rc => rc.completed == true);
-
-      if(resultTier!.length > 0 && resultCriteria!.length == reb.rebateCriteria?.length && resultTierCriteria!.length == rebTier.rebateTierCriteria!.length) {
-        reb.completed = true;
-      } else {
-        reb.completed = false;
-      }
-
-    }
-
-
-    // validate if at least one rebate is selected
-    this.validateSelection();
-  }
-
-  rebate_change(reb: Rebate) {
-    reb.rebateCriteria?.forEach(element => {
-      element.completed = reb.completed!;
-    });
-    // add rebate tier  selections TODO
-    //...
-    reb.rebateTiers?.forEach(tier => {
-      if(!reb.completed) {
-
-        tier.completed = reb.completed!;
-      } else {
-        tier.completed = tier.defaultTier;
-
-      }
-
-      tier.rebateTierCriteria?.forEach(element => {
-        element.completed = tier.completed!;
-      });
-     })
-
-
-    // validate if at least one rebate is selected
-    this.validateSelection();
-  }
-
-  reb_criteria_change(reb: Rebate) {
-    let checked_count = 0;
-
-    //Get total checked items
-    reb.rebateCriteria?.forEach(element => {
-      if (element.completed)
-        checked_count++;
-    });
-
-    if (checked_count == reb.rebateCriteria!.length) {
-      //If checked count is equal to total items; then check the master checkbox.
-      reb.completed = true;
-    } else {
-      //If none of the checkboxes in the list is checked then uncheck master.
-      reb.completed = false;
-    }
-
-      // When the user checks all of the rebate_criteria for a given rebate
-      // AND one of the rebate tiers is checked, the rebate itself will be selected.
-    const resultTier = reb.rebateTiers?.filter(rtc => rtc.completed == true);
-    const resultCriteria = reb.rebateCriteria?.filter(rc => rc.completed == true);
-      if(resultTier!.length > 0 && resultCriteria!.length == reb.rebateCriteria?.length ) {
-        reb.completed = true;
-      } else {
-        reb.completed = false;
-
-      }
-
-      // validate if at least one rebate is selected
-      this.validateSelection();
-  }
-
-  // validate if at least one rebate is selected
-  validateSelection() {
-
-    const mySelectedRebates = this.availableRebates?.filter(r => r.completed == true);
-    if(mySelectedRebates.length > 0) {
-      this.IsValidAvailabeRebates = true;
-    } else {
-      this.IsValidAvailabeRebates = false;
-    }
-
-  }
-
-  /* PAYLOAD FORMART -> [ { "rebateId": 1, "rebateTierId": 1, "required": true },
-                          { "rebateId": 2, "required": true } ] */
-  getRebatesPayload() { 
-
-    let getformat!: any;
-    let collectFormat: Array<JSON> = [];  
-
-
-    // available Rebates selected (completed = true)
-    this.availableRebates?.filter( e =>{
-
-      if (e.completed == true){       
-
-        // available Rebates Tier selected (completed = true)
-          e.rebateTiers?.filter(e2 => {
-            if (e2.completed == true){
-              getformat =  {"rebateId": e.rebateId, "rebateTierId": e2.rebateTierId, "isRequired": false};
-              collectFormat.push(getformat);
-            }
-          });
-        
-        
-      }
-
-    });
-    return collectFormat;
-    
-  }
-
   submitForm() {  
 
     this.payload = {
@@ -488,7 +234,6 @@ export class HeatingCoolingRComponent implements OnInit {
       nominalSize: this.nominalSizeGroup.value,
       fuelSource: this.furnaceGroup.controls['fuelSource'].value,
       state: this.stateGroup.value,
-      requiredRebates: this.getRebatesPayload(),
       elegibilityQuestions: this.elegibilityQuestionsGroup.value
     }  
     /* sent the infor to product-lines-components */
